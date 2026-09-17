3307ff4b7e63b3ce2264fc64d8b121cfdef96ff0
D4N73 <wagnerdante2@gmail.com>
2026-09-17 09:57:56 -0300

    Carrega normalizador de entrada na v0.5

--- RoboHoras.ps1
+++ RoboHoras.ps1
@@ -10,4 +10,5 @@ $Root = Split-Path -Parent $MyInvocation.MyCommand.Path
 Set-Location $Root
 . (Join-Path $Root "src\bootstrap.ps1")
 . (Join-Path $Root "src\excel.ps1")
+. (Join-Path $Root "src\normalize_input.ps1")
 . (Join-Path $Root "src\main.ps1")
